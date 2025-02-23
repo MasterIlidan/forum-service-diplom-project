9ac0a5e369825088894baa911f708d148f8b7d96
MasterIlidan <segamil2001@gmail.com>
2025-02-23 15:20:59 +0500

    Отображение списка пользователей

--- src/main/java/ru/students/forumservicediplomproject/controller/SecurityController.java
+++ src/main/java/ru/students/forumservicediplomproject/controller/SecurityController.java
@@ -1,17 +1,9 @@
 package ru.students.forumservicediplomproject.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import ru.students.forumservicediplomproject.dto.UserDto;
 import ru.students.forumservicediplomproject.entity.Role;
-import ru.students.forumservicediplomproject.entity.User;
 import ru.students.forumservicediplomproject.repository.RoleRepository;
-import ru.students.forumservicediplomproject.service.UserService;
 
 import java.util.List;
 
@@ -19,11 +11,8 @@ import java.util.List;
 public class SecurityController {
 
     private final RoleRepository roleRepository;
-    private final UserService userService;
 
-    public SecurityController(UserService userService,
-                              RoleRepository roleRepository) {
-        this.userService = userService;
+    public SecurityController(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
 
@@ -33,93 +22,13 @@ public class SecurityController {
         return "login";
     }
 
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        UserDto user = new UserDto();
-        model.addAttribute("user", user);
-        return "/register";
-    }
-
-    @PostMapping("/register/save")
-    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
-                               BindingResult result,
-                               Model model) {
-        User existingUser = userService.findUserByEmail(userDto.getEmail());
-
-        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
-            result.rejectValue("email", null,
-                    "User with this email already exist");
-        }
-
-        if (result.hasErrors()) {
-            model.addAttribute("user", userDto);
-            return "/register";
-        }
-        userService.saveNewUser(userDto);
-        return "redirect:/register?success";
-    }
-
-    @GetMapping("/users")
-    public String users(Model model) {
-        List<UserDto> users = userService.findAllUsers();
-        List<Role> roles = roleRepository.findAll();
-
-        String[] roleNames = {"ADMIN", "USER", "READ_ONLY"};
-
-        model.addAttribute("roleNames", roleNames);
-        model.addAttribute("users", users);
-        model.addAttribute("roles", roles);
-        return "/users";
-    }
 
     @GetMapping("/roles")
     public List<Role> roles() {
         return roleRepository.findAll();
     }
 
-/*  @PostMapping("/userUpdate")
-    public String changeUserRole(@ModelAttribute UserUpdate userUpdate) {
-        if (userUpdate == null) return "redirect:/users";
-        Optional<User> optionalUser = userService.findUserById(userUpdate.getId());
-        User existingUser;
-        if (optionalUser.isPresent()) {
-            existingUser = optionalUser.get();
-        } else {
-            return "redirect:/users";
-        }
-        List<Role> roles = existingUser.getRoles();
-        roles.set(0, new Role(userUpdate.getRole()));
-        existingUser.setRoles(existingUser.getRoles());
-        //List<Role> roles = new ArrayList<>();
 
-        //Role role = new Role(requestRoles);
-        //roles.add(role);
-        //user.setRoles(roles);
-        userService.saveUser(existingUser);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/userUpdate{id}")
-    public ModelAndView updateUser(@RequestParam @PathVariable Long id) {
-        ModelAndView mav = new ModelAndView("user-edit-form");
-        Optional<User> optionalUser = userService.findUserById(id);
-        User user = new User();
-        if (optionalUser.isPresent()) {
-            user = optionalUser.get();
-        }
-        UserUpdate userUpdate = new UserUpdate();
-
-        userUpdate.setId(user.getId());
-        userUpdate.setUsername(user.getUsername());
-        userUpdate.setEmail(user.getEmail());
-        userUpdate.setRole(user.getRoles().get(0).getRoleName());
-        //String[] roleNames = {"ADMIN", "USER", "READ_ONLY"};
-        List<String> roleNames = List.of(new String[]{"ADMIN", "USER", "READ_ONLY"});
-
-        mav.addObject("roleNames", roleNames);
-        mav.addObject("user", userUpdate);
-        return mav;
-    }*/
     @GetMapping("/about")
     public String about(){
         return "/about";
--- src/main/java/ru/students/forumservicediplomproject/controller/UserController.java
+++ src/main/java/ru/students/forumservicediplomproject/controller/UserController.java
@@ -1,16 +1,19 @@
 package ru.students.forumservicediplomproject.controller;
 
-import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.students.forumservicediplomproject.Search;
 import ru.students.forumservicediplomproject.dto.UserDto;
+import ru.students.forumservicediplomproject.entity.Role;
 import ru.students.forumservicediplomproject.entity.User;
 import ru.students.forumservicediplomproject.service.UserService;
+import ru.students.forumservicediplomproject.service.UserServiceImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,8 +25,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user{userId}")
-    public ModelAndView userProfile(@PathVariable @Nullable Long userId, Model model) {
+    @GetMapping("/users")
+    public ModelAndView users(Model model) {
+        ModelAndView modelAndView = new ModelAndView("users-list");
+        List<UserDto> users = userService.findAllUsers();
+        //List<Role> roles = roleRepository.findAll();
+
+        UserServiceImpl.UserRoles[] roleNames = UserServiceImpl.UserRoles.values();
+
+        modelAndView.addObject("roleNames", roleNames);
+        modelAndView.addObject("users", users);
+        //modelAndView.addObject("roles", roles);
+        return modelAndView;
+    }
+
+    @GetMapping("/user/{userId}")
+    public ModelAndView userProfile(@PathVariable Long userId, Model model) {
         ModelAndView modelAndView = new ModelAndView("profile-page");
         Optional<User> user;
         if (userId == null) {
@@ -50,9 +67,79 @@ public class UserController {
 
         return modelAndView;
     }
-    @GetMapping("/userProfile{email}")
+    @GetMapping("/user/{email}")
     public String userProfile(@PathVariable String email) {
         User user = userService.findUserByEmail(email);
         return "redirect:/user?userId=%s".formatted(user.getUserId());
+    }
+
+    @PostMapping("/user/")
+    public String changeUserRole(@ModelAttribute UserDto userUpdate) {
+        if (userUpdate == null) return "redirect:/users";
+        Optional<User> optionalUser = userService.findUserById(userUpdate.getId());
+        User existingUser;
+        if (optionalUser.isPresent()) {
+            existingUser = optionalUser.get();
+        } else {
+            return "redirect:/users";
+        }
+        List<Role> roles = existingUser.getRoles();
+        //roles.set(0, new Role(userUpdate.getRole()));
+        existingUser.setRoles(existingUser.getRoles());
+        //List<Role> roles = new ArrayList<>();
+
+        //Role role = new Role(requestRoles);
+        //roles.add(role);
+        //user.setRoles(roles);
+        userService.saveUser(existingUser);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/register")
+    public String registrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "/register";
+    }
+
+    @PostMapping("/register/save")
+    public String saveUserAfterRegistration(@Valid @ModelAttribute("user") UserDto userDto,
+                                            BindingResult result,
+                                            Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null,
+                    "User with this email already exist");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+        userService.saveNewUser(userDto);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/user/{id}/editForm")
+    public ModelAndView updateUser(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("user-edit-form");
+        Optional<User> optionalUser = userService.findUserById(id);
+        User user = new User();
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+        }
+        UserDto userUpdate = new UserDto();
+
+        userUpdate.setId(user.getUserId());
+        userUpdate.setUsername(user.getUserName());
+        userUpdate.setEmail(user.getEmail());
+        //userUpdate.setRoles(user.getRoles().get(0).getRoleName());
+        //String[] roleNames = {"ADMIN", "USER", "READ_ONLY"};
+        List<String> roleNames = List.of(new String[]{"ADMIN", "USER", "READ_ONLY"});
+
+        mav.addObject("roleNames", roleNames);
+        mav.addObject("user", userUpdate);
+        return mav;
     }
 }
